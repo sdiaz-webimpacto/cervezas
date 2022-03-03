8aac70fd40541d2a55a7daf34352f4ad5d8b89db
Santi <santidiazto@gmail.com>
2022-03-03 18:45:29 +0100

    cambios estilos dinamicos en plantilla

--- front/views/js/template.js
+++ front/views/js/template.js
@@ -1,7 +1,20 @@
 $.ajax({
 	url:"ajax/template.ajax.php",
 	success:function(respuesta){
-		const topBar = JSON.parse(respuesta).topBar;
-		console.log(JSON.parse(respuesta));
+		const topBar = JSON.parse(respuesta)[0][0].top_bar;
+		const topBarText = JSON.parse(respuesta)[0][0].top_bar_text;
+		const header = JSON.parse(respuesta)[0][0].header;
+		const backcolor = JSON.parse(respuesta)[0][0].backcolor;
+		const backcolorText = JSON.parse(respuesta)[0][0].backcolor_text;
+
+		$('header').css('background', header);
+		$('.topBar, .topBar a').css({
+			'background-color': topBar,
+			'color': topBarText
+		});
+		$('.backColor, .backColor a').css({
+			'background-color': backcolor,
+			'color': backcolorText
+		});
 	}
 })
