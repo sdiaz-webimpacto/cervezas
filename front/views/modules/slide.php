3a25fa586e5ff57df0fee9d64334363d996f2a32
Santi <sdiaz@webimpacto.es>
2022-03-11 18:37:26 +0100

    finalizacion de maquetación slider

--- front/views/modules/slide.php
+++ front/views/modules/slide.php
@@ -1,20 +1,62 @@
 <div class="container-fluid" id="slide">
     <div class="row">
         <ul>
-            <li>
+
+            <li class="slideShow">
                 <img src="<?php echo $path;?>back/views/img/slider/slider1.jpg">
                 <div class="slideOptions slideOption1">
-                    <img src="<?php echo $path;?>back/views/img/slider/producto.png" alt="" class="imgProduct">
-                    <div class="textSlide">
-                        <h1>Title</h1>
-                        <h2>Subtitle</h2>
-                        <h3>Comment for this product</h3>
+                    <img src="<?php echo $path;?>back/views/img/slider/producto.png" alt="" class="imgProduct" style="top:2%;left:60%;width:auto">
+                    <div class="textSlide" style="top:12%;left:10%;width:40%">
+                        <h1 style="color:#ccc;">Title</h1>
+                        <h2 style="color:#ccc;">Subtitle</h2>
+                        <h3 style="color:#ccc;">Comment for this product</h3>
                         <a href="">
                             <button class="btn btn-default backColor text-uppercase">Show Product <span class="fa fa-chevron-right"></span></button>
                         </a>
                     </div>
                 </div>
             </li>
+
+            <li class="slideShow">
+                <img src="<?php echo $path;?>back/views/img/slider/slider2.jpg">
+                <div class="slideOptions slideOption2">
+                    <img src="<?php echo $path;?>back/views/img/slider/producto.png" alt="" class="imgProduct" style="top:2%;left:10%;width:auto">
+                    <div class="textSlide" style="top:12%;left:55%;width:40%">
+                        <h1 style="color:#ccc;">Title</h1>
+                        <h2 style="color:#ccc;">Subtitle</h2>
+                        <h3 style="color:#ccc;">Comment for this product</h3>
+                        <a href="">
+                            <button class="btn btn-default backColor text-uppercase">Show Product <span class="fa fa-chevron-right"></span></button>
+                        </a>
+                    </div>
+                </div>
+            </li>
+
+            <li class="slideShow">
+                <img src="<?php echo $path;?>back/views/img/slider/slider3.jpg">
+                <div class="slideOptions slideOption2">
+                    <img src="<?php echo $path;?>back/views/img/slider/producto.png" alt="" class="imgProduct" style="top:2%;left:10%;width:auto">
+                    <div class="textSlide" style="top:12%;left:55%;width:auto">
+                        <h1 style="color:#ccc;">Title</h1>
+                        <h2 style="color:#ccc;">Subtitle</h2>
+                        <h3 style="color:#ccc;">Comment for this product</h3>
+                        <a href="">
+                            <button class="btn btn-default backColor text-uppercase">Show Product <span class="fa fa-chevron-right"></span></button>
+                        </a>
+                    </div>
+                </div>
+            </li>
+
         </ul>
+
+        <ul id="pagination">
+            <li item="1"><span class="fa fa-circle"></span></li>
+            <li item="2"><span class="fa fa-circle"></span></li>
+            <li item="3"><span class="fa fa-circle"></span></li>
+        </ul>
+
+        <div class="arrows" id="back"><span class="fa fa-chevron-left"></span></div>
+        <div class="arrows" id="next"><span class="fa fa-chevron-right"></span></div>
+
     </div>
 </div>
